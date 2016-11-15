f04bef292872bff73f8cb63fafc77bda9b0ced07
Sebastian Green-Husted <ricecake@tfm.nu>
2016-11-14 19:20:07 -0500

    export the new funcitons

--- src/watchbin.erl
+++ src/watchbin.erl
@@ -3,6 +3,7 @@
 -export([
 	start/0,
 	new/2,
+	new/3,
 	destroy/1,
 	start_timer/4,
 	stop_timer/2
@@ -11,15 +12,16 @@
 start() -> application:ensure_all_started(watchbin).
 
 new(BucketSize, Callback) ->
-	{ok, Pid} = watchbin_srv:new(BucketSize, Callback),
-	{ok, {watchbin, Pid}}.
+	watchbin_srv:new(BucketSize, Callback).
 
-destroy({watchbin, Pid}) ->
+new(BucketSize, Callback, Label) ->
+	watchbin_srv:new(BucketSize, Callback, Label).
+
+destroy(Pid) ->
 	watchbin_srv:destroy(Pid).
 
-start_timer({watchbin, Pid}, Interval, Data, Opts) when is_integer(Interval), is_list(Opts) ->
+start_timer(Pid, Interval, Data, Opts) when is_integer(Interval), is_list(Opts) ->
 	watchbin_worker:start_timer(Pid, Interval, Data, Opts).
 
-stop_timer({watchbin, Pid}, TimerID) ->
+stop_timer(Pid, TimerID) ->
 	watchbin_worker:stop_timer(Pid, TimerID).
-
